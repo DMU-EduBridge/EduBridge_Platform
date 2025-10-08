daa3ba6fe68e7759eb1395468ccff3810db809db
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2025-10-08 18:42:31 +0900

    chore(학습): my/learning 페이지 정리

--- src/app/(afterLogin)/my/learning/page.tsx
+++ src/app/(afterLogin)/my/learning/page.tsx
@@ -24,7 +24,7 @@ export default async function MyLearningPage() {
   }
 
   return (
-    <div className="space-y-6 p-4">
+    <div className="space-y-6 p-6">
       <MyLearningClient />
     </div>
   );
